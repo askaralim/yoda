ea71bb1a89c1483a62de42751ce99d3c551e94cc
Askar <>
2025-07-31 17:08:07 +0800

    rocletmqtemplate conflict

--- src/main/java/com/taklip/yoda/config/RocketMQConfig.java
+++ src/main/java/com/taklip/yoda/config/RocketMQConfig.java
@@ -1,5 +1,6 @@
 package com.taklip.yoda.config;
 
+// import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Role;
@@ -13,4 +14,10 @@ public class RocketMQConfig {
     public MessageConverter messageConverter() {
         return new MappingJackson2MessageConverter();
     }
+
+    // @Bean
+    // @Role(org.springframework.beans.factory.config.BeanDefinition.ROLE_INFRASTRUCTURE)
+    // public RocketMQTemplate rocketMQTemplate() {
+    //     return new RocketMQTemplate();
+    // }
 }
