094761e45088879d59bf8444dec37a8d856a05b2
Askar <>
2025-08-01 16:26:15 +0800

    testing

--- src/main/java/com/taklip/yoda/config/AsyncConfig.java
+++ src/main/java/com/taklip/yoda/config/AsyncConfig.java
@@ -15,7 +15,7 @@ public class AsyncConfig {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(5);
         executor.setMaxPoolSize(10);
-        executor.setQueueCapacity(25);
+        executor.setQueueCapacity(100);
         executor.setThreadNamePrefix("Async-");
         executor.initialize();
         return executor;
